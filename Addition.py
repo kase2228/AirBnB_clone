8a0dd0e115bb238b0272d53f2b676606ed1f67ab
etel <etelethiopia@gmail.com>
2024-03-10 09:40:59 -0700

    files

--- Addition.py
+++
@@ -1,9 +0,0 @@
-def add_numbers(a: int, b: int) -> int:
-    """
-    Adds two numbers together.
-
-    :param a: The first number.
-    :param b: The second number.
-    :return: The sum of the two numbers.
-    """
-    return a + b
